15e0619819a10499ea2c95a02f15c3406c668aaf
jayachandra1209 <jaya2chandra@gmail.com>
2020-04-28 18:46:08 +0530

    read command edited file

--- 04-read.sh
+++ 04-read.sh
@@ -2,4 +2,6 @@
 
 read -p 'Enter your name: ' name
 
-echo -e "Name=$name"
+echo  "Name=$name"
+
+# Read command is not used in Automation. Only when run the scripts manually we will use read command.
