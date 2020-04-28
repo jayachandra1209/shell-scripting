b727ec5ef3c526191d82ead45952376af08d6d41
jayachandra1209 <jaya2chandra@gmail.com>
2020-04-28 18:57:39 +0530

    added $values

--- 05-Special_variable.sh
+++ 05-Special_variable.sh
@@ -1,3 +1,8 @@
 # Special Variables are $0-$n(0,1,2,3,...n) $* $@ ,$# are used for taking inputs
 
-echo $0
+#will give the file name
+echo $0  
+
+#first argument is $1
+echo $1
+echo $2 #argument2
