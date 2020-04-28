48db8d06b75d0a1d3a22f582fe9c806c7e2ba16e
jayachandra1209 <jaya2chandra@gmail.com>
2020-04-28 16:14:55 +0530

    Problem statement file added

---
+++ 03-problem-statement.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Following Countries are Lockdown"
+echo "1.China"
+echo "2.Italy"
+echo "3.India"
+
+echo "Following countries are not lock down"
+echo "1.USA"
+echo "2.Russia"
