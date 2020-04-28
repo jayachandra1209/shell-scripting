c594feefbd511a10577e158e509326d8edc70e26
jayachandra1209 <jaya2chandra@gmail.com>
2020-04-28 13:48:25 +0530

    adding new file name as pring.sh

---
+++ 02-print.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## to print some messsage on the screen we choose printing commands
+#there are 2 commands which are 
+#1. echo
+#2. printf
+
+# we choose  to go with echo as it is reduces the syntax problems unlike printf.
+
+echo Hello world
