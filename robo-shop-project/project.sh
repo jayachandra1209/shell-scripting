84072d5fa886ca406f9c86a6e24f7f4830782ecf
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-05 17:56:51 +0530

    disabling the code before enabling

--- robo-shop-project/project.sh
+++ robo-shop-project/project.sh
@@ -1,16 +1,16 @@
 #/bin/bash
 
-RB="\e[1;31m"
-GB="\e[1;32m"
-YB="\e[1;33m"
-BB="\e[1;34m"
-MB="\e[1;35m"
-CB="\e[1;36m"
+RB="\e[0m\e[1;31m"
+GB="\e[0m\e[1;32m"
+YB="\e[0m\e[1;33m"
+BB="\e[0m\e[1;34m"
+MB="\e[0m\e[1;35m"
+CB="\e[0m\e[1;36m"
 N="\e[0m"
-D="\e[2m"
-B="\e[1m"
+D="\e[0m\e[2m"
+B="\e[0m\e[1m"
 ##RB-Red Bold, GB-Green Bold,---and so on
 #N--> Nutralize the color
 #D-->varaibale for dim color
 #B-->Variable for bold color
-echo -e "${BB}[MONGO]${N}[INFO]${N} ${D}$(date +%F' '%T)${N} ${D}${B}MONGODB INSTALL${N}"
+echo -e "${BB}[MONGO][INFO] ${D}$(date +%F' '%T)${B}MONGODB INSTALL${N}"
