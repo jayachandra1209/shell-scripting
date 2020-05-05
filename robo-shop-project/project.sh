ca506d7e469f92dbd5250eaaebba766901f8f4a1
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-05 18:07:50 +0530

    added color lines for all the services

--- robo-shop-project/project.sh
+++ robo-shop-project/project.sh
@@ -13,4 +13,15 @@ B="\e[0m\e[1m"
 #N--> Nutralize the color
 #D-->varaibale for dim color
 #B-->Variable for bold color
-echo -e "${BB}[MONGO]${B}[INFO] ${D}$(date +%F' '%T) ${B}MONGODB INSTALL${N}"
+echo -e "${BB}[MONGODB]${B}[INFO] ${D}$(date +%F' '%T) ${B}MONGODB INSTALL${N}"
+echo -e "${BB}[RABITMQ]${B}[INFO] ${D}$(date +%F' '%T) ${B}RABITMQ INSTALL${N}"
+echo -e "${BB}[MYSQL]${B}[INFO] ${D}$(date +%F' '%T) ${B}MYSQL INSTALL${N}"
+echo -e "${BB}[REDIS]${B}[INFO] ${D}$(date +%F' '%T) ${B}REDIS INSTALL${N}"
+
+echo -e "${MB}[NGINX]${B}[INFO] ${D}$(date +%F' '%T) ${B}NGINX INSTALL${N}"
+
+echo -e "${CB}[CATALOGUE]${B}[INFO] ${D}$(date +%F' '%T) ${B}CATALOGUE INSTALL${N}"
+echo -e "${CB}[CART]${B}[INFO] ${D}$(date +%F' '%T) ${B}CART INSTALL${N}"
+echo -e "${CB}[USER]${B}[INFO] ${D}$(date +%F' '%T) ${B}USER INSTALL${N}"
+echo -e "${CB}[SHIPPING]${B}[INFO] ${D}$(date +%F' '%T) ${B}SHIPPING INSTALL${N}"
+echo -e "${CB}[PAYMENT]${B}[INFO] ${D}$(date +%F' '%T) ${B}PAYMENT INSTALL${N}"
