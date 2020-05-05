01b377756563e3d0818b5e4740ce27c81fc2242b
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-05 16:48:12 +0530

    just comment added to exit-status.sh file

--- 06-exit-status.sh
+++ 06-exit-status.sh
@@ -10,3 +10,5 @@
 ## System by default will store the last command of exit status in (?)questionmark varaibla.
  
  exit 1
+
+ ## Values from 126-255 are used by system , Hence it is not  recomended to use those values.
