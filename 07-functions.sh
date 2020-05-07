7835f5c2f6226eeee4d1b24c491a16df9b45a0c3
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-07 12:39:01 +0530

    added some function related things

--- 07-functions.sh
+++ 07-functions.sh
@@ -34,3 +34,28 @@ SAMPLE2
 ## return commands works like exit command for a function, Hence it can come with an exit status of a function.
 # return command can return value from 0-255 and as same like exit command.
 
+SAMPLE3() {
+  echo Hello22
+  return 99
+}
+
+SAMPLE3 
+echo "Exit Status of SAMPLE3 = $?"
+
+## function can take the arguments like a normal script. Meaning $1-$9, $* , $@,$# are available for functions.
+# $0 is not available. $0 is exclusivly reserved for shell script name.
+
+SAMPLE4() {
+  echo "First Arg = $1"
+  echo "Second Arg = $2"
+}
+
+SAMPLE4 10 20
+
+## Function is one type of command. Linux OS will have four types of commands.
+
+## Variable behaviour in function 
+## 1. Declare a variable in main program and you can access that in function 
+## 2. Declare a variable in function and you can access in main program.
+## 3. Variables from main program can be overriden by function and vice-versa.
+
