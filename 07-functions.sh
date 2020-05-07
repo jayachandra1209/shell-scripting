e7f9912414c53e8c52bb235f947d41e709bcb3bc
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-07 12:34:17 +0530

    added execute function line

--- 07-functions.sh
+++ 07-functions.sh
@@ -9,3 +9,17 @@
 function SAMPLE() {
   echo Hello from Sample function 
 }
+
+## Method 2 
+
+SAMPLE1() {
+  echo Hello from Sample1 Function
+}
+
+## Access function 
+SAMPLE # to call SAMPLE function 
+SAMPLE1 # to call SAMPLE1 function
+
+## We cannot use exit commands to come out of function. Exit command will exit the complete the script.
+## To come out of a function we need to use return command 
+
