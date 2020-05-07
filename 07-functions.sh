b162d95b96d13fc1041559b0ce7f0c0084030d93
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-07 12:36:33 +0530

    addind return function in function.sh

--- 07-functions.sh
+++ 07-functions.sh
@@ -23,3 +23,14 @@ SAMPLE1 # to call SAMPLE1 function
 ## We cannot use exit commands to come out of function. Exit command will exit the complete the script.
 ## To come out of a function we need to use return command 
 
+SAMPLE2() {
+  echo Hello21
+  return 
+  echo Hello22
+}
+
+SAMPLE2 
+
+## return commands works like exit command for a function, Hence it can come with an exit status of a function.
+# return command can return value from 0-255 and as same like exit command.
+
