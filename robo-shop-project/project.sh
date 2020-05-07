6934dc9058ff0960716213f2ca1f290b13721ee4
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-07 13:18:31 +0530

    added installations in project.sh

--- robo-shop-project/project.sh
+++ robo-shop-project/project.sh
@@ -13,15 +13,134 @@ B="\e[0m\e[1m"
 #N--> Nutralize the color
 #D-->varaibale for dim color
 #B-->Variable for bold color
-echo -e "${BB}[MONGODB]${B}[INFO] ${D}$(date +%F' '%T) ${B}MONGODB INSTALL${N}"
-echo -e "${BB}[RABITMQ]${B}[INFO] ${D}$(date +%F' '%T) ${B}RABITMQ INSTALL${N}"
-echo -e "${BB}[MYSQL]${B}[INFO] ${D}$(date +%F' '%T) ${B}MYSQL INSTALL${N}"
-echo -e "${BB}[REDIS]${B}[INFO] ${D}$(date +%F' '%T) ${B}REDIS INSTALL${N}"
+#echo -e "${BB}[MONGODB]${B}[INFO] ${D}$(date +%F' '%T) ${B}MONGODB INSTALL${N}"
+#echo -e "${BB}[RABITMQ]${B}[INFO] ${D}$(date +%F' '%T) ${B}RABITMQ INSTALL${N}"
+#echo -e "${BB}[MYSQL]${B}[INFO] ${D}$(date +%F' '%T) ${B}MYSQL INSTALL${N}"
+#echo -e "${BB}[REDIS]${B}[INFO] ${D}$(date +%F' '%T) ${B}REDIS INSTALL${N}"
 
-echo -e "${MB}[NGINX]${B}[INFO] ${D}$(date +%F' '%T) ${B}NGINX INSTALL${N}"
+#echo -e "${MB}[NGINX]${B}[INFO] ${D}$(date +%F' '%T) ${B}NGINX INSTALL${N}"
 
-echo -e "${CB}[CATALOGUE]${B}[INFO] ${D}$(date +%F' '%T) ${B}CATALOGUE INSTALL${N}"
-echo -e "${CB}[CART]${B}[INFO] ${D}$(date +%F' '%T) ${B}CART INSTALL${N}"
-echo -e "${CB}[USER]${B}[INFO] ${D}$(date +%F' '%T) ${B}USER INSTALL${N}"
-echo -e "${CB}[SHIPPING]${B}[INFO] ${D}$(date +%F' '%T) ${B}SHIPPING INSTALL${N}"
-echo -e "${CB}[PAYMENT]${B}[INFO] ${D}$(date +%F' '%T) ${B}PAYMENT INSTALL${N}"
+#echo -e "${CB}[CATALOGUE]${B}[INFO] ${D}$(date +%F' '%T) ${B}CATALOGUE INSTALL${N}"
+#echo -e "${CB}[CART]${B}[INFO] ${D}$(date +%F' '%T) ${B}CART INSTALL${N}"
+#echo -e "${CB}[USER]${B}[INFO] ${D}$(date +%F' '%T) ${B}USER INSTALL${N}"
+#echo -e "${CB}[SHIPPING]${B}[INFO] ${D}$(date +%F' '%T) ${B}SHIPPING INSTALL${N}"
+#echo -e "${CB}[PAYMENT]${B}[INFO] ${D}$(date +%F' '%T) ${B}PAYMENT INSTALL${N}"
+
+LOG_FILE=/tmp/project.log
+rm -f LOG_FILE
+
+LOGGER() {
+    
+    case $1 in
+    INFO)
+    STAT_COLOR=${B}
+    ;;
+    FAIL)
+    STAT_COLOR=${RB}
+    ;;
+    SUCC)
+    STAT_COLOR=${GB}
+    ;;
+    SKIP) 
+      STAT_COLOR=${YB}
+   esac
+
+    case $SERVICE_NAME in 
+    MONGODB|RABBITMQ|MYSQL|REDIS)
+    echo -e "${D}$(date +%F' '%T) ${STAT_COLOR}[$1] ${BB}[$SERVICE_NAME] ${B}${2}${N}"
+    ;;
+    NGINX)
+    echo -e "${D}$(date +%F' '%T) ${STAT_COLOR}[$1] ${MB}[$SERVICE_NAME] ${B}${2}${N}"
+    ;;
+    CATALOGUE|CART|USER|SHIPPING|DISPATCH|PAYMENT)
+    echo -e "${D}$(date +%F' '%T) ${STAT_COLOR}[$1] ${CB}[$SERVICE_NAME] ${B}${2}${N}"
+    ;;
+    esac
+    }
+
+STAT() {
+  case $1 in 
+    SKIP) 
+      LOGGER SKIP "$2"
+      ;;
+    0) 
+      LOGGER SUCC "$2"
+      ;; 
+    *) 
+      LOGGER FAIL "$2"
+      ;;
+  esac
+}
+
+## Main Program
+SERVICE_NAME=MONGODB
+LOGGER INFO "Starting MongoDB Setup"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb-org-4.2.repo 
+STAT $? "Setting Up YUM Repository"
+
+yum install -y mongodb-org  &>>$LOG_FILE 
+STAT $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE 
+systemctl start mongod &>>$LOG_FILE 
+STAT $? "Starting MongoDB Service"
+
+
+SERVICE_NAME=RABBITMQ
+LOGGER INFO "Starting RABBITMQ Setup"
+yum list installed | grep erlang &>/dev/null
+case $? in 
+  0) 
+    STAT SKIP "Installing ErLang Package"
+    ;;
+  *) 
+    yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y   &>>$LOG_FILE 
+    STAT $? "Installing ErLang Package"
+    ;;
+esac
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE 
+STAT $? "Setting up Yum repos"
+
+yum install rabbitmq-server -y &>>$LOG_FILE 
+STAT $? "Installing RabbitMQ Server"
+
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl start rabbitmq-server &>>$LOG_FILE
+STAT $? "Starting RabbitMQ Server"
+
+
+
+
+SERVICE_NAME=MYSQL 
+LOGGER INFO "Starting MYSQL Setup"
+
+yum remove mariadb-libs -y &>/dev/null
+yum list installed | grep mysql-community-server &>/dev/null
+case $? in 
+  0) 
+    STAT SKIP "Downloading MYSQL"
+    ;;
+  *) 
+    LOGGER INFO "Downloading MYSQL"
+    wget https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar  -O /tmp/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar &>>$LOG_FILE 
+    STAT $? "Downloading MYSQL"
+    cd /tmp
+    tar -xf mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar 
+    yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm \
+              mysql-community-common-5.7.28-1.el7.x86_64.rpm \
+              mysql-community-libs-5.7.28-1.el7.x86_64.rpm \
+              mysql-community-server-5.7.28-1.el7.x86_64.rpm -y &>>$LOG_FILE 
+    STAT $? "Installing MySQL Database"
+    rm -rf mysql-5* *.rpm
+    ;;
+esac
+systemctl enable mysqld &>>$LOG_FILE 
+systemctl start mysqld &>>$LOG_FILE 
+STAT $? "Starting MySQL Database"
+
