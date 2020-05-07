9c9aa699d9d3a364a08723731540b4cf0f86a416
jayachandra1209 <jaya2chandra@gmail.com>
2020-05-07 12:23:14 +0530

    added function.sh file

---
+++ 07-functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+## if we assign a name to set of data then that is a variable 
+## id we assign a name to set of commands then that is a function
+
+## Declare a function 
+
+## Method 1 
+function SAMPLE() {
+  echo Hello from Sample function 
+}
